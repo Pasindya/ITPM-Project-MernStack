057aaa5076a57bf4badc396466580376235563e8
Pasindya <pasindyachavindi13@gmail.com>
2025-03-13 12:41:05 +0530

    modified

--- Backend/app.js
+++ Backend/app.js
@@ -5,10 +5,11 @@ const router = require("./Routes/HBookingRoute")
 const app = express();
 
 //Middleware
+app.use(express.json());
 app.use("/hbookings",router);
 
 //database connection
-mongoose.connect("mongodb+srv://travel:1234@cluster0.nitw6zk.mongodb.net/")
+mongoose.connect("mongodb+srv://thisurasenawin:1234@cluster0.b8mmc.mongodb.net/")
 .then(()=> console.log("Connected to MongoDB Successfully"))
 .then(() => {
     app.listen(5000);
