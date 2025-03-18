8910271baf32cb4fb067a80c30711d0f8dfa4e21
Pasindya <pasindyachavindi13@gmail.com>
2025-03-18 21:26:52 +0530

    Set weather tracker succesfully

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Welcome from './Pages/Welcome'; // Import the Welcome page
 import Home from './Pages/Home';
 import Footer from './Components/Footer';
 import Packages from './Pages/Packages';
+import Weather from './Pages/Weather';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Welcome />} /> {/* Welcome page as the home route */}
         <Route path="/home" element={<Home />} />
         <Route path="/packages" element={<Packages />} />
+        <Route path="/weather" element={<Weather />} />
       
       </Routes>
       <Footer /> {/* Include the Footer here */}
