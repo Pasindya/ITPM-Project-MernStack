f1a4ffc4f6ffde102e24a06bbc80d15d435eb9e7
SanduniVandana <168699888+SanduniVandana@users.noreply.github.com>
2025-03-29 13:19:43 +0800

    transport 4
    ID

--- Frontend/src/Pages/TransportNav.jsx
+++ Frontend/src/Pages/TransportNav.jsx
@@ -130,7 +130,7 @@ function TransportNav() {
             </Link>
           </li>
           <li>
-            <Link to="/updatevehicle" style={styles.sidebarLink} onClick={() => setIsOpen(false)}>
+            <Link to="/" style={styles.sidebarLink} onClick={() => setIsOpen(false)}>
               <FaEdit style={styles.icon} /> Edit Vehicle Bookings
             </Link>
           </li>
